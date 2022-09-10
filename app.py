2c5b173feec58960bdf82cdff7e718b9c21156ae
morris <kajimone@gmail.com>
2022-09-10 12:05:11 -0400

    parms

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/test")
@@ -7,7 +7,8 @@ def home():
 
 @app.route("/search")
 def search():
-    return "This is a json format result"
+    problemType = request.args.get('type')
+    return problemType;
 
 if __name__ == '__main__':
     app.run()
